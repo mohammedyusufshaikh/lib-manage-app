2d4bbbbf1b84c7d3d8fd1fb1d1cd739e109dd9b0
Yusuf <iyusufshaikh78@gmail.com>
2021-03-22 21:13:06 +0530

    database uri update

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 class Config:
     SECRET_KEY = '23e717009be83a9e03fdf08e4abb84a0'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/library'
+    SQLALCHEMY_DATABASE_URI = 'mysql://c0mrade:root@c0mrade.mysql.pythonanywhere-services.com/library'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
+
