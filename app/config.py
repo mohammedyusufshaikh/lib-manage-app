c82592d9e0d3d706d0a7beb541442cb603451f62
Yusuf <iyusufshaikh78@gmail.com>
2021-03-22 21:28:32 +0530

    database pass

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = '23e717009be83a9e03fdf08e4abb84a0'
-    SQLALCHEMY_DATABASE_URI = 'mysql://c0mrade:root@c0mrade.mysql.pythonanywhere-services.com/c0mrade$library'
+    SQLALCHEMY_DATABASE_URI = 'mysql://c0mrade:Pass@6689@c0mrade.mysql.pythonanywhere-services.com/c0mrade$library'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
